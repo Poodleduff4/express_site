0e7f4d76c201595b103cca98d9aebec6c67a34cc
luke guardino <lukeguardino@yahoo.ca>
2021-07-12 23:27:38 -0400

    suggestions working

--- imdb/movies.js
+++ imdb/movies.js
@@ -4,19 +4,19 @@ const { Console } = require('console');
 const { CONNREFUSED } = require('dns');
 var https = require('https')
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     movieTitle = req.query.t;
     res.send(movieTitle);
 });
 
-router.get('/suggestions', function(req, res) {
+router.get('/suggestions', function (req, res) {
     movieTitle = req.query.t;
     var host = 'v2.sg.media-imdb.com'
     var filename = '/suggestion/' + movieTitle[0] + '/' + movieTitle + '.json';
     var url = 'https://' + host + filename;
     console.log(url);
     const suggestions = [];
-    https.get(url, function(response){
+    https.get(url, function (response) {
 
         response.on('data', function (d) {
             // let json = JSON.stringify(d);
@@ -24,25 +24,31 @@ router.get('/suggestions', function(req, res) {
             //     // var value = json[key];
             //     console.log(key);
             //   }
-            let temp = JSON.parse(d);
-            for(var key in temp['d']) {
-                for(var key2 in temp['d'][key]){
-                    // console.log(key + '/' + key2 + ': ' + temp['d'][key][key2]);
-                    
+            try {
+                let temp = JSON.parse(d);
+
+
+                for (var key in temp['d']) {
+                    for (var key2 in temp['d'][key]) {
+                        // console.log(key + '/' + key2 + ': ' + temp['d'][key][key2]);
+
+                    }
+                    // let tempTitle = temp['d'][key]['l'];
+                    suggestions.push(temp['d'][key]['l']);
+                    // console.log(suggestions);
+                    // console.log(temp['d'][key]['l']);
+                    // console.log('\n');
+
                 }
-                // let tempTitle = temp['d'][key]['l'];
-                suggestions.push(temp['d'][key]['l']);
                 // console.log(suggestions);
-                // console.log(temp['d'][key]['l']);
-                // console.log('\n');
-                
-              }
-              console.log(suggestions);
-              res.send(suggestions);
+                res.send(suggestions);
+            } catch (error) {
+                console.error(error);
+            }
         });
     });
     // console.log(suggestions);
-    
+
 });
 
 module.exports = router;
